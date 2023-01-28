f762ec6a78272b373e37206561d6c019decd521d
Bryan <bryanparisot@hotmail.com>
2023-01-28 10:32:44 +0100

    feat(utilitaire): add new pages

--- pages/utilitaires.jsx
+++ pages/utilitaires.jsx
@@ -1,19 +1,33 @@
 import { Client } from "@notionhq/client";
 import Link from "next/link";
+import Announces from "../components/Announces/Announces";
+import HeaderWithImgTitle from "../components/Navigation/HeaderWithImgTitle";
 
 export default function Utilitaire({ vehicle }) {
-  console.log(vehicle);
   return (
     <div>
-      <h1>Notion with Next.js</h1>
-      <div>
-        {vehicle.map((item, index) => (
-          <div key={item.id}>
-            <Link href={`/vehicle/${item.id}`}>
-              <p> {item.properties?.description?.rich_text[0]?.plain_text}</p>
-            </Link>
-          </div>
-        ))}
+      <HeaderWithImgTitle />
+      <div className="p-20">
+        <div>
+          {vehicle.map((item, index) => (
+            <div key={item.id}>
+              <Link href={`/vehicle/${item.id}`}>
+                {console.log(item.properties?.images?.files[0]?.file.url)}
+                <Announces
+                  image={item.properties?.images?.files[0]?.file.url}
+                  title={item.properties?.nom_du_vehicule?.title[0]?.plain_text}
+                  description={
+                    item.properties?.description?.rich_text[0]?.plain_text
+                  }
+                  vitesse={
+                    item.properties?.type_boite_vitesse?.rich_text[0]
+                      ?.plain_text
+                  }
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
